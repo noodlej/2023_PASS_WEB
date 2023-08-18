bd63c4d1b1ae6123326a22f97447741eecf275e0
uniqquej <cjy4477@gmail.com>
2023-08-18 16:13:32 +0900

    code for issue #2

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,7 @@ module.exports = {
         console.log(req);
         if(req.isAuthenticated()) {
             console.log('로그인 된 사용자입니다.');
+            console.log('로그인 수정')
             next();
         }
         else {
